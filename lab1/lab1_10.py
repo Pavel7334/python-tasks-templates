dda19dd2b5bfe836a0102cc25a07ef4fd87006d7
Игонин Павел <88630513+Pavel7334@users.noreply.github.com>
2023-03-15 13:35:06 +0400

    completed the task lab1_10

--- lab1/lab1_10.py
+++ lab1/lab1_10.py
@@ -4,7 +4,16 @@
 
 
 def app(str1: str) -> None:
-    ... # Ваш код
+    print(str1[::-1])
+
+    print("".join(reversed(str1)))
+
+    chars = list(str1)
+    for i in range(len(str1) // 2):
+        tmp = chars[i]
+        chars[i] = chars[len(str1) - i - 1]
+        chars[len(str1) - i - 1] = tmp
+    print(''.join(chars))
 
 
 if __name__ == '__main__':
