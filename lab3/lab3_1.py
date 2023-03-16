0f22fbb70b7eaff0d26e1ce8a36aac43e62bbd01
Игонин Павел <88630513+Pavel7334@users.noreply.github.com>
2023-03-16 12:02:31 +0400

    completed the tasks lab3_1 and lab3_2

--- lab3/lab3_1.py
+++ lab3/lab3_1.py
@@ -1,6 +1,6 @@
 # Дан кортеж my_tuple = (1, 2, 5, 6). Выведите в терминал его размер.
 def app(my_tuple: tuple) -> None:
-    ... # Ваш код
+    print(len(my_tuple))
 
 
 if __name__ == "__main__":
--- lab3/lab3_2.py
+++ lab3/lab3_2.py
@@ -1,7 +1,8 @@
 # Распакуйте кортеж my_tuple = (1, 6) в переменные value1 и value2. 
 # Выведите в терминал полученный результат через пробел - 1 6.
 def app(my_tuple: tuple) -> None:
-    ... # Ваш код
+    value1, value2 = my_tuple
+    print(value1, value2)
 
 
 if __name__ == "__main__":
