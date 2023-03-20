143818f31dd6a88d7ebd6448d2eaac59962cb473
Игонин Павел <88630513+Pavel7334@users.noreply.github.com>
2023-03-20 17:54:57 +0400

    completed the tasks lab5_5 and lab5_6

--- lab5/lab5_6.py
+++ lab5/lab5_6.py
@@ -1,7 +1,8 @@
 # Дан словарь my_dict = {1: 10, 'a': 5, 'b': -2}. 
 # Удалите из него «'a': 5» и выведите в терминал полученный результат. 
 def app(my_dict: dict) -> None:
-    ... # Ваш код
+    popped_value = my_dict.pop('a')
+    print(my_dict)
 
 
 if __name__ == "__main__":
