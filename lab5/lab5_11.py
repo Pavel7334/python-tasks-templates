ee1e7affababbdda154c8d55d8d6bfe8d84d0b6e
Игонин Павел <88630513+Pavel7334@users.noreply.github.com>
2023-03-20 19:39:22 +0400

    completed the tasks lab5_11 and lab5_12

--- lab5/lab5_11.py
+++ lab5/lab5_11.py
@@ -2,10 +2,13 @@
 # Напишите программу, формирующую словарь, где в качестве ключей 
 # выступают элементы первого списка, а в качестве значений - второго. 
 def app(my_list1: list, my_list2: list) -> None:
-    ... # Ваш код
+    new_dict = {}
+    for i, sym in enumerate(my_list1):
+        new_dict[sym] = new_dict.get(sym, 0) + my_list2[i]
+    print(new_dict)
 
 
 if __name__ == "__main__":
     my_list1 = [1, 2, 3, 4, 9, 7, 4]
-    my_list2 = [2, 13, 4, 8, 7, 6]
+    my_list2 = [2, 13, 4, 8, 7, 6, 5]
     app(my_list1, my_list2)
