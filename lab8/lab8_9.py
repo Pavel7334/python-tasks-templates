2fdf5768f130a1a86683d96a06af7e0957aeaf1a
Игонин Павел <88630513+Pavel7334@users.noreply.github.com>
2023-03-23 13:33:56 +0400

    completed the task lab8_9

--- lab8/lab8_9.py
+++ lab8/lab8_9.py
@@ -4,9 +4,23 @@
 # или «Центр координат», «Ось X» (если y = 0) и 
 # «Ось Y» (если х=0)). Текст выводить без кавычек!
 def app():
-    x = int(input())
-    y = int(input())
-    ... # Ваш код 
+    print("Координаты точки:")
+    x = float(input("x = "))
+    y = float(input("y = "))
+    if x > 0 and y > 0:
+        print("Точка в I четверти")
+    elif x < 0 and y > 0:
+        print("Точка во II четверти")
+    elif x < 0 and y < 0:
+        print("Точка в III четверти")
+    elif x > 0 and y < 0:
+        print("Точка в IV четверти")
+    elif x == 0 and y == 0:
+        print("Точка в центре координат")
+    elif x == 0:
+        print("Точка на оси X")
+    elif y == 0:
+        print("Точка на оси Y")
 
 
 if __name__ == "__main__":
