5aeb47c1b2303ae3bc3ac4e9f08748c7c1f25bd6
Игонин Павел <88630513+Pavel7334@users.noreply.github.com>
2023-03-23 13:06:54 +0400

    completed the task lab8_7

--- lab8/lab8_7.py
+++ lab8/lab8_7.py
@@ -2,7 +2,15 @@
 # либо равно 10, то выведите в терминал сумму его первого и последнего 
 # элемента, иначе произведение первого и предпоследнего.
 def app():
-    ... # Ваш код 
+    my_list = list(map(int, input().strip().split()))
+    if my_list[len(my_list) // 2] >= 10:
+        print(my_list[0] + my_list[-1])
+    if my_list[len(my_list) // 2] < 10:
+        print(my_list[0] * my_list[-2])
+
+
+
+
 
 
 if __name__ == "__main__":
