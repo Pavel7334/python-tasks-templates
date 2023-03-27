a2e0a4478aa8e427c424242db06a9bcaa25ac228
Игонин Павел <88630513+Pavel7334@users.noreply.github.com>
2023-03-27 17:59:28 +0400

    completed the task lab9_3

--- lab9/lab9_3.py
+++ lab9/lab9_3.py
@@ -2,7 +2,15 @@
 # найдите сумму его элементов и выведите полученный 
 # результат в терминал.
 def app():
-    ... # Ваш код
+    my_list = list(map(int, input().strip().split()))
+    i = 0
+    amount = 0
+
+    # цикл вычисления суммы
+    while i < len(my_list):
+        amount = amount + my_list[i]
+        i += 1
+    print("Сумма =", amount)
 
 
 if __name__ == "__main__":
