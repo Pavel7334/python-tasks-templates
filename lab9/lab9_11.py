58b7525e3fd21bd6235dbc3629b935bd036b5622
Игонин Павел <88630513+Pavel7334@users.noreply.github.com>
2023-03-29 15:28:07 +0400

    completed the task lab9_11

--- lab9/lab9_11.py
+++ lab9/lab9_11.py
@@ -2,7 +2,14 @@
 # сумму значений, которые нацело делятся на 4 и выведите в 
 # терминал полученный результат.
 def app():
-    ... # Ваш код
+    my_list = list(map(int, input().strip().split()))
+    i = 0
+    summa = 0
+    while i < len(my_list):
+        if my_list[i] % 4 == 0:
+            summa += my_list[i]
+        i += 1
+    print(summa)
 
 
 if __name__ == "__main__":
