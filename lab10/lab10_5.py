d50adc3e877e91a78236a32a0ae8b3664540d408
Игонин Павел <88630513+Pavel7334@users.noreply.github.com>
2023-03-30 15:07:47 +0400

    completed the task lab10_5

--- lab10/lab10_5.py
+++ lab10/lab10_5.py
@@ -2,7 +2,9 @@
 # Используя механизм списковых включений сформируйте единичную матрицу N×N 
 # и выведите полученный результат в терминал.
 def app():
-    ... # Ваш код
+    size = int(input())
+    matrix = list([[1 if i == j else 0 for j in range(size)] for i in range(size)])
+    print(*matrix, sep='\n')
 
 
 if __name__ == "__main__":
