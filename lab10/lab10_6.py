89f34861c7223f71187c0108601661a715509d5e
Игонин Павел <88630513+Pavel7334@users.noreply.github.com>
2023-03-30 15:12:31 +0400

    completed the task lab10_6

--- lab10/lab10_6.py
+++ lab10/lab10_6.py
@@ -2,7 +2,8 @@
 # возведите все его элементы в квадрат и выведите полученный 
 # результат в терминал.
 def app():
-    ... # Ваш код
+    my_list = list(map(int, input().strip().split()))
+    print([el ** 2 for el in my_list])
 
 
 if __name__ == "__main__":
