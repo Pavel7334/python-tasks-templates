c155cf21d0a489015bb9141e967af4deebb3e438
Игонин Павел <88630513+Pavel7334@users.noreply.github.com>
2023-04-03 15:19:07 +0400

    completed the task lab11_6

--- lab11/lab11_6.py
+++ lab11/lab11_6.py
@@ -1,8 +1,13 @@
 #Напишите функцию find_dict_key, на вход которой подается 
 # словарь и искомое в нем значение и возвращающую ключ по 
 # которому оно хранится. Если элемента с искомым 
-# значением нет – верните None. 
-... # Ваш код
+# значением нет – верните None.
+def find_dict_key(my_dict, value):
+    for k, new_va in my_dict.items():
+        if value == new_va:
+            return k
+        else:
+            return None
 
 
 if __name__ == "__main__":
