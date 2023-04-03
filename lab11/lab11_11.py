4056267947b23f9a4f65e15a34a9d3b70f956f89
Игонин Павел <88630513+Pavel7334@users.noreply.github.com>
2023-04-03 17:32:30 +0400

    completed the task lab11_11

--- lab11/lab11_11.py
+++ lab11/lab11_11.py
@@ -2,7 +2,22 @@
 # Функция должна осуществлять проверку, являются ли переданные 
 # значения анаграммами или нет, после чего вернуть 
 # результат проверки (True – да, нет – False).
-... # Ваш код
+def is_anagram(str1, str2):
+    list1 = list(str1)
+    list2 = list(str2)
+    list1.sort()
+    list2.sort()
+
+    position = 0
+    matches = True
+
+    while position < len(str1) and matches:
+        if list1[position] == list2[position]:
+            position = position + 1
+        else:
+            matches = False
+
+    return matches
 
 
 if __name__ == "__main__":
