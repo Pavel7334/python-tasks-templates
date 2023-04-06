21a28188f3c4a610befae1d596c910d9e1496394
Игонин Павел <88630513+Pavel7334@users.noreply.github.com>
2023-04-06 17:12:14 +0400

    completed the task lab11_23

--- lab11/lab11_23.py
+++ lab11/lab11_23.py
@@ -4,12 +4,51 @@
 # если он соблюден, то вернуть значение – True, 
 # иначе – False. Когда в строке отсутствуют скобки должно возвращаться True.
 def bracket(string: str) -> bool:
+
     while '()' in string:
         string = string.replace('()', '')
     return string == ''
+
+
+
 
 
 if __name__ == "__main__":
     my_str = input("Введите строку со скобками ")
 
     print(bracket(my_str))
+
+
+def check_string(string: str) -> bool:
+    stack = []
+    for s in string:
+        if s == ')' and not stack:
+            return False
+        if s == '(':
+            stack.append(s)
+        elif s == ')':
+            try:
+                stack.pop()
+            except IndexError:
+                return False
+    return len(stack) == 0
+
+
+print(check_string('(((())'))
+
+
+def check_string(string: str) -> bool:
+    flag = 0
+    for s in string:
+        if s == ')' and flag == 0:
+            return False
+        if s == '(':
+            flag += 1
+        elif s == ')':
+            flag -= 1
+        if flag < 0:
+            return False
+    return flag == 0
+
+
+print(check_string('(()))'))
