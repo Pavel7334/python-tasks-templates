189c1a8c434a04673f53354dd827f9782a7f6880
Игонин Павел <88630513+Pavel7334@users.noreply.github.com>
2023-04-07 13:30:26 +0400

    completed the task lab12_1

--- lab12/lab12_1.py
+++ lab12/lab12_1.py
@@ -2,7 +2,9 @@
 # подается целочисленное значение n. Необходимо вычислить 
 # сумму всех чисел от 1 до n.
 
-# Ваш код
+def recursion_sum(num: int) -> int:
+    return num + recursion_sum(num-1) if num else 0
+
 
 if __name__ == "__main__":
     val1 = int(input("Введите значение n "))
