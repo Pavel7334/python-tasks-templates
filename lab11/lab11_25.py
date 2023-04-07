b5ce6a91aad9b9ea93c59210cc5b45dad49364d3
Игонин Павел <88630513+Pavel7334@users.noreply.github.com>
2023-04-07 13:24:03 +0400

    completed the task lab11_25

--- lab11/lab11_25.py
+++ lab11/lab11_25.py
@@ -4,7 +4,8 @@ from math import pow
 # Напишите функцию algebraic_sum, на вход которой 
 # подается два значения N и k (по умолчанию равно 2). 
 # Функция должна возвращать результат следующего выражения: 1^k + 2^k + 3^k + … + N^k.
-... # Ваш код
+def algebraic_sum(n: int, k: int = 2) -> list:
+    return [int(pow(el, k)) for el in range(n)]
 
 
 if __name__ == "__main__":
