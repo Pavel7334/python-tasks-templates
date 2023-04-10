a80104b439aeac7df51435f583ccf76d99b25029
Игонин Павел <88630513+Pavel7334@users.noreply.github.com>
2023-04-10 14:03:47 +0400

    completed the task lab12_4

--- lab12/lab12_4.py
+++ lab12/lab12_4.py
@@ -1,8 +1,13 @@
 # Напишите рекурсивную функцию recursion_palindrom, на вход которой 
 # подается строка. Если строка представляет собой палиндром, 
-# то функция должна вернуть значение – True, иначе – False.  
+# то функция должна вернуть значение – True, иначе – False.
 
-# Ваш код
+def recursion_palindrom(my_str: str) -> bool:
+    if my_str[0] != my_str[-1]:
+        return False
+    if len(my_str) > 2:
+        return recursion_palindrom(my_str[1:-1])
+    return True
 
 
 if __name__ == "__main__":
