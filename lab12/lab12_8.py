fd448d043c07ae413169859ddbfdd00662aa8cc4
Игонин Павел <88630513+Pavel7334@users.noreply.github.com>
2023-04-11 21:53:22 +0400

    completed the task lab12_8

--- lab12/lab12_8.py
+++ lab12/lab12_8.py
@@ -2,9 +2,27 @@
 # удаляющую из строки все заданные буквы и возвращающую 
 # результирующую строку.
 
-# Ваш код 
+def remove_v(my_str: str, el: str) -> str:
+    # new_1 = ''
+    # count = 0
+    # for i in my_str:
+    #     if i != el:
+    #         new_1 += i
+
+    # while count < len(my_str):
+    #     if my_str[count] != el:
+    #         new_1 += my_str[count]
+    #     count += 1
+
+    if not my_str:
+        return ''
+    if my_str[0] == el:
+        result = remove_v(my_str[1:], el)
+        return result
+    result = my_str[0] + remove_v(my_str[1:], el)
+    return result
 
 
 if __name__ == "__main__":
-    my_str = "Приyyyyвет"
+    my_str = "Приуууувет"
     print(remove_v(my_str, 'у'))
