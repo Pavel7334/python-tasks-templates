445808ec4370fab770ee966882e3c122c3d76b77
Игонин Павел <88630513+Pavel7334@users.noreply.github.com>
2023-04-15 13:07:19 +0400

    completed the task lab13_4

--- lab13/lab13_4.py
+++ lab13/lab13_4.py
@@ -2,8 +2,11 @@
 # для сложения двух чисел и возвращающую полученное значение. 
 # Например, closure_sum(1)(2) -> 3.
 
-# Ваш код 
+def closure_sum(a):
+    def helper(b):
+        return a + b
 
+    return helper
 
 if __name__ == "__main__":
     print(closure_sum(1)(2))
