e1abeb9b15948cf0c996846b2dc0559ab88efb0f
Игонин Павел <88630513+Pavel7334@users.noreply.github.com>
2023-04-17 15:41:51 +0400

    completed the task lab14_1

--- lab14/lab14_1.py
+++ lab14/lab14_1.py
@@ -2,14 +2,17 @@
 # результирующее значение (элементы списка) функции sum_list, 
 # на вход которой подается целочисленный список и возвращается сумма его элементов.
 
-# Код декоратора
+def pow_list_result(func):
+    def wrapper(my_list):
+        return func(my_list) ** 2
+    return wrapper
 
 
 @pow_list_result
-# Код функции
+def sum_list(my_list1):
+    return sum(my_list1)
 
 
 if __name__ == "__main__":
     my_list1 = [2, 2, 2, 4]
-    print(sum_list(my_list1)) # 100
-
+    print(sum_list(my_list1))  # 100
