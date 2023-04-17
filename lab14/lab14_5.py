e4d6bd385de2294193babe905396561a79d7e6d1
Игонин Павел <88630513+Pavel7334@users.noreply.github.com>
2023-04-17 18:42:34 +0400

    completed the task lab14_5

--- lab14/lab14_5.py
+++ lab14/lab14_5.py
@@ -5,12 +5,19 @@
 # На вход функции del_str подается строка и символ, который из нее надо удалить.
 
 
-# Код декоратора
+def str_case_result(func):
+    def wrapper(my_str, el):
+        new_str = my_str[:2].upper() + my_str[len(my_str) // 2 - 1] + my_str[-2:].lower()
+        return func(new_str, el)
+
+    return wrapper
 
 
 @str_case_result
-# Код функции
+def del_str(my_str: str, el) -> str:
+    res_str = my_str.replace(el, '')
+    return res_str
 
 
 if __name__ == "__main__":
-    print(del_str("qwerty", "r")) # QWety
+    print(del_str("qwerty", "r"))  # QWety
