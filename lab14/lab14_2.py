1f2ff20dd1d1c9fdeec1c203ae5c7a83476931fc
Игонин Павел <88630513+Pavel7334@users.noreply.github.com>
2023-04-17 17:22:08 +0400

    completed the task lab14_2

--- lab14/lab14_2.py
+++ lab14/lab14_2.py
@@ -3,13 +3,18 @@
 # подается целочисленный список и возвращается его найденное максимальное значение.
 from math import sqrt
 
-# Код декоратора
+
+def sqrt_list_result(func):
+    def wrapper(my_list):
+        return sqrt(func(my_list))
+    return wrapper
 
 
 @sqrt_list_result
-# Код функции
+def max_number(my_list: list) -> int:
+    return max(my_list)
 
 
 if __name__ == "__main__":
     my_list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    print(max_number(my_list1)) # 3.0
+    print(max_number(my_list1))  # 3.0
