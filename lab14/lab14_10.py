d133882e5b41d0d49986325ec5fa3a1e89c9a89b
Игонин Павел <88630513+Pavel7334@users.noreply.github.com>
2023-04-21 17:56:25 +0400

    completed the task lab14_10

--- lab14/lab14_10.py
+++ lab14/lab14_10.py
@@ -3,14 +3,23 @@
 # возвращающий полученное значение. В качестве декорируемой 
 # напишите функцию add, вычисляющий сумму двух поступающих на ее вход значений.
 
+def mul_result(n: int):
+    def new_decorator(func):
 
-def mul_result(func):
-    def helper()
+        def helper(*args, **kwargs):
+            print(*args, **kwargs)
+            result = func(*args, **kwargs) * n
+            return result
+
+        return helper
+
+    return new_decorator
 
 
 @mul_result(2)
-def add
+def add(num1: int, num2: int) -> int:
+    return num1 + num2
 
 
 if __name__ == "__main__":
-    print(add(3, 5)) # 16
+    print(add(3, 5))  # 16
